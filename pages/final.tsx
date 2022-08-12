2d62e9f41fe7bd118b60805e2f1661d8442c5f5b
nebular-lab <hiranodaisuke0721@gmail.com>
2022-08-12 13:27:16 +0900

    layout5

--- pages/final.tsx
+++ pages/final.tsx
@@ -95,7 +95,7 @@ const Final = () => {
           <Text fontSize="50px">が向いています</Text>
         </Flex>
         <Box bg="gray.100" borderRadius="10px" p="5px" h="35%" w="50%">
-          <Text fontSize="10px" whiteSpace="pre-wrap" lineHeight="20px">
+          <Text fontSize="15px" whiteSpace="pre-wrap" lineHeight="20px">
             {desText}
           </Text>
           <Link href={bookLink} target="_blank">
